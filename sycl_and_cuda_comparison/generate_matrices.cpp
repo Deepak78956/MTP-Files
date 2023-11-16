80e057b62834c0d35a2ef1e405efd5b29b24558e
Deepak <dk7895625828@gmail.com>
2023-11-16 04:45:49 +0000

    Modified SYCL matmul

---
+++ sycl_and_cuda_comparison/generate_matrices.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+// Define the value of N using a macro
+#define N 2048
+
+int main()
+{
+    // Seed the random number generator
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    // Create two matrices of size N x N and populate them with random values
+    int **matrixA, **matrixB;
+    matrixA = (int **)malloc(N * sizeof(int *));
+
+    // Allocate memory for each row (an array of integers)
+    for (int i = 0; i < N; i++)
+    {
+        matrixA[i] = (int *)malloc(N * sizeof(int));
+    }
+
+    matrixB = (int **)malloc(N * sizeof(int *));
+
+    // Allocate memory for each row (an array of integers)
+    for (int i = 0; i < N; i++)
+    {
+        matrixB[i] = (int *)malloc(N * sizeof(int));
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrixA[i][j] = std::rand() % 100; // Generate random values between 0 and 99
+            matrixB[i][j] = std::rand() % 100;
+        }
+    }
+
+    // Open a file for writing (overwrite if it already exists)
+    std::ofstream outputFile("output.txt");
+
+    if (!outputFile)
+    {
+        std::cerr << "Error opening the output file." << std::endl;
+        return 1;
+    }
+
+    // Write N to the file
+    outputFile << N << " " << N << "\n";
+
+    // Write the values of matrix A to the file
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            outputFile << matrixA[i][j] << " ";
+        }
+    }
+    outputFile << "\n";
+
+    // Write the values of matrix B to the file
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            outputFile << matrixB[i][j] << " ";
+        }
+    }
+
+    // Close the output file
+    outputFile.close();
+
+    std::cout << "Matrices written to 'output.txt'" << std::endl;
+
+    return 0;
+}
--- sycl_and_cuda_comparison/matmul_sycl.cpp
+++ sycl_and_cuda_comparison/matmul_sycl.cpp
@@ -2,40 +2,59 @@
 #include <array>
 #include <iostream>
 #include <string>
-#define N 100
 using namespace std;
 
-void matrix_mutilply(sycl::queue &q, int *A, int *B, int *C)
-{
-    q.submit([&](sycl::handler &h)
-             { h.parallel_for(sycl::range{N, N}, [=](sycl::id<2> index)
-                              {
-            int i = index[0];
-            int j = index[1];
+#define THREADS 32
 
-            for (int k = 0; k < N; k++){
-                C[i * N + j] += A[i * N + k] * B[k * N + j];
+void matrix_mutilply(sycl::queue &q, int *A, int *B, int *C, unsigned int N)
+{
+    q.submit([&](sycl::handler &cgh){
+        sycl::range<2> globalRange{ N , N };
+        sycl::range<2> localRange{ THREADS, THREADS };
+
+        cgh.parallel_for(sycl::nd_range<2>(globalRange, localRange), [=](sycl::nd_item<2> item){
+            size_t i = item.get_group(1) * THREADS + item.get_local_id(1);
+            size_t j = item.get_group(0) * THREADS + item.get_local_id(0);
+
+            float sum = 0.0f;
+            for (size_t k = 0; k < N; ++k) {
+                sum += A[i * N + k] * B[k * N + j];
+            }
+
+            C[i * N + j] = sum;
+        });
+    });
+    q.wait_and_throw();
+    // q.submit([&](sycl::handler &h)
+    //          { h.parallel_for(sycl::range{N, N}, [=](sycl::id<2> index)
+    //                           {
+    //         int i = index[0];
+    //         int j = index[1];
+
+    //         for (int k = 0; k < N; k++){
+    //             C[i * N + j] += A[i * N + k] * B[k * N + j];
                 
-                        } }); })
-        .wait();
-}
-
-void fillWithRandom(int *arr)
-{
-    int numRange = 10;
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            arr[i * N + j] = rand() % numRange;
-        }
-    }
-    cout << endl;
+    //                     } }); })
+    //     .wait();
 }
 
 int main()
 {
+
+    int M, N;
+
+    // Open the file for reading
+    std::ifstream inputFile("output.txt");
+
+    if (!inputFile)
+    {
+        std::cerr << "Error opening the input file." << std::endl;
+        return 1;
+    }
+
+    // Read the header from the file
+    inputFile >> M >> N;
+
     int *mat_A, *mat_B, *mat_C, *dev_res;
 
     sycl::queue q{sycl::gpu_selector{}};
@@ -45,8 +64,19 @@ int main()
     mat_C = (int *)malloc(sizeof(int) * (N * N));
     dev_res = (int *)malloc(sizeof(int) * (N * N));
 
-    fillWithRandom(mat_A);
-    fillWithRandom(mat_B);
+    // Read the values of matrix A from the file
+    for (int i = 0; i < N * N; i++)
+    {
+        inputFile >> mat_A[i];
+    }
+
+    // Read the values of matrix B from the file
+    for (int i = 0; i < N * N; i++)
+    {
+        inputFile >> mat_B[i];
+    }
+
+    inputFile.close();
 
     int *dev_mat_A, *dev_mat_B, *dev_mat_C;
 
@@ -54,16 +84,23 @@ int main()
     dev_mat_B = sycl::malloc_device<int>((N * N), q);
     dev_mat_C = sycl::malloc_device<int>((N * N), q);
 
-    q.submit([&](sycl::handler &cgh)
-             { cgh.memcpy(dev_mat_A, mat_A, N * N * sizeof(int)); });
-    q.submit([&](sycl::handler &cgh)
-             { cgh.memcpy(dev_mat_B, mat_B, N * N * sizeof(int)); });
+    q.memcpy(dev_mat_A, mat_A,  N * N * sizeof(int)).wait();
+    q.memcpy(dev_mat_B, mat_B,  N * N * sizeof(int)).wait();
 
-    matrix_mutilply(q, dev_mat_A, dev_mat_B, dev_mat_C);
-    cout << endl;
+    // q.submit([&](sycl::handler &cgh)
+    //          { cgh.memcpy(dev_mat_A, mat_A, N * N * sizeof(int)); });
+    // q.submit([&](sycl::handler &cgh)
+    //          { cgh.memcpy(dev_mat_B, mat_B, N * N * sizeof(int)); });
 
-    q.submit([&](sycl::handler &cgh)
-             { cgh.memcpy(dev_res, dev_mat_C, N * N * sizeof(int)); });
+    clock_t calcTime;
+    calcTime = clock();
+    matrix_mutilply(q, dev_mat_A, dev_mat_B, dev_mat_C, N);
+    calcTime = clock() - calcTime;
+
+    q.memcpy(dev_res, dev_mat_C,  N * N * sizeof(int)).wait();
+
+    // q.submit([&](sycl::handler &cgh)
+    //          { cgh.memcpy(dev_res, dev_mat_C, N * N * sizeof(int)); });
 
     int flag = 0;
 
@@ -93,6 +130,14 @@ int main()
     {
         cout << "Check Complete: Correct Answer" << endl;
     }
+
+    cout << "COMPLETED SUCCESSFULLY\n";
+    double t_time = ((double)calcTime) / CLOCKS_PER_SEC * 1000;
+    cout << t_time << endl;
+    cout << endl;
+
+    auto maxWorkGroupSize = q.get_device().get_info<sycl::info::device::max_work_group_size>();
+    std::cout << "Maximum Work-Group Size: " << maxWorkGroupSize << std::endl;
 
     return 0;
 }
